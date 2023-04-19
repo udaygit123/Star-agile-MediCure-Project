982ee00c838fe8b718b764d87ffe24cf871fcd9b
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-19 20:02:53 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -24,6 +24,21 @@ resource "aws_instance" "Medicure-Deploy" {
   provisioner "local-exec" {
         command = " echo ${aws_instance.Medicure-Deploy.public_ip} >> inventory.txt "
   }
+   provisioner "remote-exec" {
+    inline = [
+             "sudo yum update -y",
+             "sudo yum install docker -y",
+             "sudo systemctl start docker",
+             "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+             "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+             "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
+             "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+             "sudo chmod +x /home/ubuntu/kubectl",
+             "sudo cp kubectl /usr/local/bin/kubectl",
+             "sudo groupadd docker",
+             "sudo usermod -aG docker ec2-user",
+    ]
+  }
    provisioner "local-exec" {
   command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/Playbook.yml "
   } 
