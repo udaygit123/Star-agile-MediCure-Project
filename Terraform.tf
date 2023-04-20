280c94d1ad245959d415c73f8ed2f585846b0f1d
MinimalKushal <120809383+MinimalKushal@users.noreply.github.com>
2023-04-20 11:57:20 +0530

    Update Terraform.tf

--- Terraform.tf
+++ Terraform.tf
@@ -37,27 +37,10 @@ resource "aws_instance" "Medicure-Deploy" {
               "sudo microk8s status",
               "sudo microk8s kubectl create deployment medicure-deploy --image=minimalkushal/medicure",
               "sudo microk8s kubectl expose deployment medicure-deploy --port=8082 --type=NodePort",
-              "sudo kubectl get svc",
-             #"sudo yum install docker -y",
-             #"sudo systemctl start docker",
-             #"sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-             #"sudo chmod +x /home/ec2-user/minikube-linux-amd64",
-             #"sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-             #"curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
-             #"sudo chmod +x /home/ec2-user/kubectl",
-             #"sudo cp kubectl /usr/local/bin/kubectl",
-             #"sudo groupadd docker",
-             #"sudo usermod -aG docker ec2-user",
-             #"sudo minikube start --force",
-             #"sudo kubectl apply -f /var/lib/jenkins/workspace/Medicure/deployment.yml",
-             #"sudo kubectl apply -f /var/lib/jenkins/workspace/Medicure/svc.yml",
-             #"sudo kubectl create deployment medicure --image=minimalkushal/medicure",
-             #"sudo kubectl expose deployment medicure --type=NodePort --port=8082 --name=medicure-svc",
-             #"sudo kubectl port-forward service/medicure-svc 8082:8082",
+              "sudo microk8s kubectl get svc",
+              "sudo echo Public IP Address of the Instance",
+              "sudo curl http://checkip.amazonaws.com",
     ]
   }
- # provisioner "local-exec" {
-  #   command = "ansible-playbook /var/lib/jenkins/workspace/Medicure/Playbook.yml "
-      #command = "sudo kubectl apply -f /var/lib/jenkins/workspace/Medicure/deployment.yml"
-   #} 
 }
+
